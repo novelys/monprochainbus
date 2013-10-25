8e637c6f13f1553490448567e197812820c9dd43
Yann Klis <yann.klis@novelys.com>
2013-10-25 22:35:01 +0200

    Revert "multi-threading BABY YEAH"
    This reverts commit a35e2f657e5c05dafa550f42fa6b74a3bc988285.

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -3,19 +3,9 @@ class LinesController < ApplicationController
   def index
     stop_ids = params[:stop_ids].present? ? params[:stop_ids] : [ params[:stop_id] ]
     @stops = Stop.find stop_ids
-
-    threads = []
-    @lines = []
-    mutex = Mutex.new
-    @stops.each{|stop|
-      threads << Thread.new {
-        res = stop.lines(number: 3)
-        mutex.synchronize {
-          @lines += res
-        }
-      }
+    @lines = @stops.inject([]){|ary, stop|
+      ary += stop.lines(number: 3)
     }
-    threads.each {|t| t.join}
 
     respond_to do |format|
       format.html
