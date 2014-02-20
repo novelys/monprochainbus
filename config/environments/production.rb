e9384b95504d6f8c35706f41a55584fd82e2e929
Yann Klis <yann.klis@novelys.com>
2014-02-20 13:33:27 +0100

    use https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Monprochainbus::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-   config.action_controller.asset_host = "//#{ENV['FOG_HOST']}"
+  config.action_controller.asset_host = "https://#{ENV['FOG_HOST']}"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
